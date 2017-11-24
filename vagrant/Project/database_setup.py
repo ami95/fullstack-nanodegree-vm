d774bcfa89c47e4ee0b3964062f5ad43f39ab0d4
ami95 <mihaiadrian1995@gmail.com>
2017-11-24 13:30:15 +0200

    Create and run database_setup.py

---
+++ vagrant/Project/database_setup.py
@@ -0,0 +1,42 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+'''used in the config and class code '''
+from sqlalchemy.ext.declarative import declarative_base
+'''create our fk-relationships & lso used when we write our mapper '''
+from sqlalchemy.orm import relationship
+'''used 4 config code @ the end of this file''' 
+from sqlalchemy import create_engine
+
+'''lets sqlalchemy know that our classes are special sqlalchemy classes that 
+corespond to tables in our DB'''
+Base = declarative_base() 
+
+
+class Restaurant(Base):
+
+	__tablename__ = 'restaurant'
+
+	name = Column(String(80), nullable = False)
+	id = Column(Integer, primary_key = True)
+
+
+class MenuItem(Base):
+
+	__tablename__ = 'menu_item'
+
+	name = Column(String(80), nullable = False)
+	id = Column(Integer, primary_key = True)
+	course = Column(String(250))
+	description = Column(String(250))
+	price = Column(String(8))
+	'''look inside the restaurant table and retrieve the id nr. 
+	whenever I ask for restaurant_id '''
+	restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+	restaurant = relationship(Restaurant)
+
+
+'''point to the DB we will use & creates new .db file'''
+engine = create_engine('sqlite:///restaurantmenu.db')
+
+'''goes into the DB and adds tha classes we will soon create, as new tables'''
+Base.metadata.create_all(engine)
