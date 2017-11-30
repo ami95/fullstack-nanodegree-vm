3292e758285af28ddc67971106cce33bfd6d5a51
ami95 <mihaiadrian1995@gmail.com>
2017-11-30 16:23:06 +0200

    Build website in Flask

--- vagrant/Project/database_setup.py
+++ vagrant/Project/database_setup.py
@@ -34,6 +34,17 @@ class MenuItem(Base):
 	restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
 	restaurant = relationship(Restaurant)
 
+	@property
+	def serialize(self):
+		#Returns object data in easy serializable format
+		return{
+			'name' : self.name,
+			'description' : self.description,
+			'id' : self.id,
+			'price' : self.price,
+			'course' : self.course,
+			}
+
 
 '''point to the DB we will use & creates new .db file'''
 engine = create_engine('sqlite:///restaurantmenu.db')
